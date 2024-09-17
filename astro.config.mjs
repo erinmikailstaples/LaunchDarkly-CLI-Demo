e0621c65ff4f7b7e8290c11bda5f1f42312f2e12
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2024-09-17 19:04:54 -0400

    update errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,1 @@
-import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
-
-export default defineConfig({
-  output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  }),
-});
+export default {};
